15122ed0df289e1fc9c57588b386faadc87c673e
Balasubramani A <balasubramani23bs@gmail.com>
2025-12-21 13:25:20 +0530

    classes and objects basics add

---
+++ oops-java/oops.java
@@ -0,0 +1,24 @@
+class Pen{
+    String color;
+    String type; //ballpoint, gel, fountain etc.
+
+    public void write(){
+        System.out.println("Writing something");
+    }
+
+    public void printColor(){
+        System.out.println(this.color);
+    }
+
+    
+}
+
+public class OOPS{
+    public static void main(String[] args){
+        Pen pen1 = new Pen();
+        pen1.color = "Blue";
+        pen1.type = "Gel";
+
+        pen1.write();
+    }
+}
