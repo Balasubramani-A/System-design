bac9c2b5b682b5706d83047432a8091295c74695
Balasubramani A <balasubramani23bs@gmail.com>
2025-12-23 16:57:44 +0530

    some class changes made

--- oops-java/oops.java
+++ oops-java/oops.java
@@ -13,12 +13,34 @@ class Pen{
     
 }
 
-public class OOPS{
+class Student{//by connvension, class name always starts with capital letter while function and variable names start with small letter
+    String name;
+    int age;
+    int grade;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.grade);
+    }
+
+    public void study(){
+        System.out.println(name + " is studying");
+    }
+}
+
+
+public class oops{
     public static void main(String[] args){
         Pen pen1 = new Pen();
         pen1.color = "Blue";
         pen1.type = "Gel";
 
         pen1.write();
+
+        Student s1 = new Student();
+        s1.name = "Alice";
+        s1.age = 20;
+        s1.printInfo();
     }
 }
